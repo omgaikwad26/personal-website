ff7aaf783b8c32d294505c4ffe726199d22902ea
Om Gaikwad <omgaikwad222@gmail.com>
2025-04-01 23:45:56 -0400

    update bcg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}"
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
-  
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Adjust the path based on your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
